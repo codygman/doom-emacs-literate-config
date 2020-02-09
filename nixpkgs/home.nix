8f0d1c3c25d0e84f690b8dd9a48057ec36db41f4
Cody Goodman <cody@codygman.dev>
2020-02-08 18:09:53 -0600

    move work/personal files to syncthing

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -8,11 +8,11 @@ in
   imports = if myEnv != ""
                then if myEnv == "personal" then
                  lib.info "loading PERSONAL home manager environment"
-                   [ ./personal.nix ]
+                   [ ./Sync/personal.nix ]
                     else
                       if myEnv == "work" then
                         lib.info "loading WORK home manager environment"
-                          [ ./work.nix ]
+                          [ ./Sync/work.nix ]
                       else
                         lib.warn "MYENV is not one of 'personal' or 'work', ONLY core home environment will be available!" []
                else
@@ -38,6 +38,9 @@ in
     redshift = {
       enable = true;
       provider = "geoclue2";
+    };
+    syncthing = {
+      enable = true;
     };
   };
 }
