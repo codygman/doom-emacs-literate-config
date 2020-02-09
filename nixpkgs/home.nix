aa399417f77fcc20703902af24dfd8120c8296dc
Cody Goodman <cody@codygman.dev>
2020-02-08 23:56:31 -0600

    modify home.nix to minimum repro of unfree issue
    work.nix looks like:
    
    { config, pkgs, home, ... }:
    with import <nixpkgs> {};
    with lib;
    {
      home = {packages = with pkgs; [teams];};
    }

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -1,50 +1,9 @@
 { config, pkgs, home, ... }:
 with import <nixpkgs> {};
 with lib;
-let
-  myEnv = builtins.getEnv "MYENV";
-  emacsHEAD = import ./emacs.nix;
-in
 {
-  imports = if myEnv != ""
-               then if myEnv == "personal" then
-                 lib.info "loading PERSONAL home manager environment"
-                   [ ~/Sync/nix-home-manager-config/personal.nix ]
-                    else
-                      if myEnv == "work" then
-                        lib.info "loading WORK home manager environment"
-                          [ ~/Sync/nix-home-manager-config/work.nix ]
-                      else
-                        lib.warn "MYENV is not one of 'personal' or 'work', ONLY core home environment will be available!" []
-               else
-                 lib.warn "MYENV not specified, ONLY core home environment will be available!" [];
   nixpkgs.config.allowUnfree = true;
-  programs = {
-    home-manager.enable = true;
-    emacs = {
-      enable = true;
-      package = emacsHEAD;
-    };
-    git = {
-      enable = true;
-      userName = "Cody Goodman";
-    };
-  };
-
-  home = {
-    packages = with pkgs; [
-      ripgrep
-      source-code-pro
-    ];
-  };
-
-  services = {
-    redshift = {
-      enable = true;
-      provider = "geoclue2";
-    };
-    syncthing = {
-      enable = true;
-    };
-  };
+  imports = [ ~/Sync/nix-home-manager-config/work.nix ];
+  programs = {home-manager.enable = true;};
+  home = {packages = with pkgs; [];};
 }
