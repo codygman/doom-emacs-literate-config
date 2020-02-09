de1a717741913f352b68d3d8c21399ddfc87b011
Cody Goodman <cody@codygman.dev>
2020-02-08 23:55:12 -0600

    update paths

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -9,11 +9,11 @@ in
   imports = if myEnv != ""
                then if myEnv == "personal" then
                  lib.info "loading PERSONAL home manager environment"
-                   [ ~/Sync/personal.nix ]
+                   [ ~/Sync/nix-home-manager-config/personal.nix ]
                     else
                       if myEnv == "work" then
                         lib.info "loading WORK home manager environment"
-                          [ ~/Sync/work.nix ]
+                          [ ~/Sync/nix-home-manager-config/work.nix ]
                       else
                         lib.warn "MYENV is not one of 'personal' or 'work', ONLY core home environment will be available!" []
                else
